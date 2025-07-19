5be6a2276a53b125e23f32fbc8c7f0f28ca14746
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-07-20 02:36:52 +0600

    migrate shop

--- prisma/migrations/20250719195312_chage_offerpices/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `offerPrice` column on the `Product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Product" DROP COLUMN "offerPrice",
-ADD COLUMN     "offerPrice" DOUBLE PRECISION;
