f916f0f6941cf782d94555a6756905a3bbaefeee
uforco <srka780@gmail.com>
2025-07-22 03:27:10 +0600

    jwt time out error block

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -22,35 +22,39 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest<Request>();
+    try {
+      const request = context.switchToHttp().getRequest<Request>();
 
-    // 1. Allow if route is public
-    const isPublic = this.reflector.get<boolean>(
-      IS_PUBLIC_KEY,
-      context.getHandler(),
-    );
-    if (isPublic) return true;
+      // 1. Allow if route is public
+      const isPublic = this.reflector.get<boolean>(
+        IS_PUBLIC_KEY,
+        context.getHandler(),
+      );
+      if (isPublic) return true;
 
-    // 2. Get required roles from metadata
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (!requiredRoles) return false;
+      // 2. Get required roles from metadata
+      const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+        ROLES_KEY,
+        [context.getHandler(), context.getClass()],
+      );
+      if (!requiredRoles) return false;
 
-    // 3. Validate token and extract user info
-    //    Check if user has required roles
-    const token = this.extractBearerToken(request);
-    if (token === '') throw new UnauthorizedException();
-    const payload = await this.jwtService.verifyAsync<PayloadType>(token, {
-      secret: this.configService.get<string>('AUTHSECRET'),
-    });
-    if (!payload) {
+      // 3. Validate token and extract user info
+      //    Check if user has required roles
+      const token = this.extractBearerToken(request);
+      if (token === '') throw new UnauthorizedException();
+      const payload = await this.jwtService.verifyAsync<PayloadType>(token, {
+        secret: this.configService.get<string>('AUTHSECRET'),
+      });
+      if (!payload) {
+        throw new UnauthorizedException();
+      }
+
+      request['sub'] = payload.id;
+      return requiredRoles.some((role) => payload.role?.includes(role));
+    } catch {
       throw new UnauthorizedException();
     }
-
-    request['sub'] = payload.id;
-    return requiredRoles.some((role) => payload.role?.includes(role));
   }
 
   private extractBearerToken(req: Request): string {
