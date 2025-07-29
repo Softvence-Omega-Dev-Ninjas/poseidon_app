a94cb85bc66df6718911144c25bacb6444c18048
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-07-29 23:41:33 +0600

    remvoe pipelinea

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,7 @@ async function bootstrap() {
 
   // const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
- app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true, 
-      whitelist: true,
-      // forbidNonWhitelisted: true,
-    }),
-  );
-
+ 
   // app.useStaticAssets(join(process.cwd(), 'public'), {
   //   // Optionally set prefix if you want URL like /static/sharif.txt
   //   // prefix: '/static',
