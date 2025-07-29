6be45ef3f68b9b0188aa3f873f86147c70cf8088
uforco <srka780@gmail.com>
2025-07-30 00:17:56 +0600

    pull to new update code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerSetting } from './config/swagger';
-import { ValidationPipe } from '@nestjs/common';
 // import { NestExpressApplication } from '@nestjs/platform-express';
 // import { join } from 'path';
 
@@ -9,14 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-//  app.useGlobalPipes(
-//     new ValidationPipe({
-//       transform: true, 
-//       whitelist: true,
-//       // forbidNonWhitelisted: true,
-//     }),
-//   );
 
   // app.useStaticAssets(join(process.cwd(), 'public'), {
   //   // Optionally set prefix if you want URL like /static/sharif.txt
