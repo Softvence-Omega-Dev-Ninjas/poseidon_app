8094981438b958ed1577fdb990d8561e0d178813
uforco <srka780@gmail.com>
2025-08-03 05:50:17 +0600

    prisma error for mig

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -4,20 +4,22 @@ import { UserModule } from './user/user.module';
 // import { OrderModule } from './order/order.module';
 // import { ProductModule } from './product/product.module';
 // import { ProductCategoryModule } from './product-category/product-category.module';
-import { PostModule } from './post/post.module';
+// import { PostModule } from './post/post.module';
 // import { ImageModule } from './image/image.module';
 // import { ShopModule } from './shop/shop.module';
-import { SupporterProfileModule } from './supporter-profile/supporter-profile.module';
+// import { SupporterProfileModule } from './supporter-profile/supporter-profile.module';
+import { MembershipModule } from './membership/membership.module';
 
 @Module({
   imports: [
     UserModule,
-    SupporterProfileModule,
+    // SupporterProfileModule,
     // SupporterModule,
     // OrderModule,
     // ProductModule,
     // ProductCategoryModule,
-    PostModule,
+    // PostModule,
+    MembershipModule,
     // ShopModule,
     // ImageModule,
     // SupporterProfileModule,
