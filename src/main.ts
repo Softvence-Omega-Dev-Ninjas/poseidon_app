a85d9bde4fb426aa99f42a705297c23b5dc09f5c
uforco <srka780@gmail.com>
2025-08-08 04:43:10 +0600

    refactor code to jwt shakib

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerSetting } from './config/swagger';
-import * as dotenv from 'dotenv';
 // import { NestExpressApplication } from '@nestjs/platform-express';
 // import { join } from 'path';
 
-dotenv.config();
+// dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
- 
   // app.useStaticAssets(join(process.cwd(), 'public'), {
   //   // Optionally set prefix if you want URL like /static/sharif.txt
   //   // prefix: '/static',
