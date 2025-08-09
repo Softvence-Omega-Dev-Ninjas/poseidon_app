d5f71ccd507f4176e06ed639e447fd3b58864cab
uforco <srka780@gmail.com>
2025-08-10 02:30:35 +0600

    revase code return

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,28 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-// import { SupporterModule } from './supporter/supporter.module';
-// import { OrderModule } from './order/order.module';
-// import { ProductModule } from './product/product.module';
-// import { ProductCategoryModule } from './product-category/product-category.module';
-// import { PostModule } from './post/post.module';
-// import { ImageModule } from './image/image.module';
-// import { ShopModule } from './shop/shop.module';
-// import { SupporterProfileModule } from './supporter-profile/supporter-profile.module';
+import { SupporterModule } from './supporter/supporter.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
+import { ProductCategoryModule } from './product-category/product-category.module';
+import { PostModule } from './post/post.module';
+import { ImageModule } from './image/image.module';
+import { SupporterProfileModule } from './supporter-profile/supporter-profile.module';
 import { MembershipModule } from './membership/membership.module';
 
 @Module({
   imports: [
     UserModule,
-    // SupporterProfileModule,
-    // SupporterModule,
-    // OrderModule,
-    // ProductModule,
-    // ProductCategoryModule,
-    // PostModule,
+    SupporterProfileModule,
+    SupporterModule,
+    OrderModule,
+    ProductModule,
+    ProductCategoryModule,
+    PostModule,
     MembershipModule,
-    // ShopModule,
-    // ImageModule,
-    // SupporterProfileModule,
+    ImageModule,
+    SupporterProfileModule,
   ],
   controllers: [],
   providers: [],
