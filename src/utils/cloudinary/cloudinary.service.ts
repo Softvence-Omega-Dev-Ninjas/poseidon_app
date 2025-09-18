82579fc1adc539157263ac6e0e1e37b0892a0c91
Md SHARIF <srka780@gmail.com>
2025-09-19 01:56:15 +0600

    cover images upload system

--- src/utils/cloudinary/cloudinary.service.ts
+++ src/utils/cloudinary/cloudinary.service.ts
@@ -133,4 +133,43 @@ export class CloudinaryService {
       throw new InternalServerErrorException('Failed to upload Image');
     }
   }
+
+  async fullUploadSystemFileData(file?: Express.Multer.File) {
+    try {
+      if (!file) return { id: null };
+
+      const fileName = file.originalname.split('.');
+      const extentionName = fileName.pop();
+      const updateName =
+        fileName.join('') + new Date().toISOString() + '.' + extentionName;
+      file.originalname = updateName;
+
+      const uploadRes = await new Promise<any>((resolve, reject) => {
+        const uploadStream = this.cloudinary.uploader.upload_stream(
+          {
+            public_id: file.originalname,
+            resource_type: 'image',
+          },
+          (error, result) => {
+            if (error) return reject(error);
+            resolve(result);
+          },
+        );
+
+        streamifier.createReadStream(file.buffer).pipe(uploadStream);
+      });
+
+      const media = await this.prisma.media.create({
+        data: {
+          imageUrl: uploadRes.secure_url,
+          publicId: uploadRes.public_id,
+        },
+      });
+
+      return media;
+    } catch (err) {
+      console.error('Error uploading image:', err);
+      return { id: null };
+    }
+  }
 }
