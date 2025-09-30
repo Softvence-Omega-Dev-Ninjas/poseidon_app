cc327e5078c2642b479a6a23879c6acbd7996726
mdsharif <srka780@gmail.com>
2025-10-01 03:41:31 +0600

    permisstion message user list

--- src/main/permission/permission.controller.ts
+++ src/main/permission/permission.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { Roles } from 'src/auth/guard/roles.decorator';
 import { Role } from 'src/auth/guard/role.enum';
+import { Request } from 'express';
 
 @Controller('permissions')
 export class PermissionController {
@@ -9,13 +10,17 @@ export class PermissionController {
 
   @Roles(Role.Supporter, Role.User)
   @Get('messages/allowed-for-me')
-  async allowedSupporters(@Req() req: any) {
-    return this.permService.getMessagePermittedSupportersForUser(req?.sub);
+  async allowedSupporters(@Req() req: Request) {
+    return this.permService.getMessagePermittedSupportersForUser(
+      req['sub'] as string,
+    );
   }
 
   @Roles(Role.Supporter, Role.User)
   @Get('messages/i-can-message')
-  async iCanMessage(@Req() req: any) {
-    return this.permService.getUsersThatSupporterCanMessage(req?.sub);
+  async iCanMessage(@Req() req: Request) {
+    return this.permService.getUsersThatSupporterCanMessage(
+      req['sub'] as string,
+    );
   }
 }
