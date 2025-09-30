c086898d1bc0c491630e5bb888cc64a9ab119de6
mdsharif <srka780@gmail.com>
2025-10-01 03:32:32 +0600

    forget password system and chack valid email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,9 +5,15 @@ import * as argon2 from 'argon2';
 import { ConfigService } from '@nestjs/config';
 import { PayloadType } from './guard/jwtPayloadType';
 import { SellerService } from 'src/utils/stripe/seller.service';
-import { VarifyEmailDto } from './dto/varify.dto';
+import {
+  VarifyEmailDto,
+  ForgetPasswordToken,
+  ForgetPasswordCodeCheck,
+  CheckVarifyEmail,
+} from './dto/varify.dto';
 import { generateCode } from 'src/common/utils/generateCode';
 import { MailService } from 'src/utils/mail/mail.service';
+import { AuthUserService } from 'src/main/user/user-auth-info/authUser.service';
 
 @Injectable()
 export class AuthService {
@@ -16,6 +22,7 @@ export class AuthService {
     private readonly configService: ConfigService,
     private readonly stripeSellerService: SellerService,
     private readonly mailService: MailService,
+    private readonly authUserService: AuthUserService,
   ) {}
 
   async userCredentialsAuthentication(
@@ -97,6 +104,8 @@ export class AuthService {
     }
   }
 
+  // signup varify email
+
   async varifyemail(data: VarifyEmailDto) {
     const G4code = generateCode();
     console.log('G4code', G4code);
@@ -104,7 +113,7 @@ export class AuthService {
       { email: data.email, code: G4code },
       {
         secret: this.configService.get<string>('AUTHSECRET'),
-        expiresIn: '1m',
+        expiresIn: '2m',
       },
     );
     const subject = 'Email Verification';
@@ -140,24 +149,15 @@ export class AuthService {
     };
   }
 
-  async checkVarifyEmail({
-    token,
-    email,
-    code,
-  }: {
-    token: string;
-    email: string;
-    code: number;
-  }) {
+  async checkVarifyEmail(data: CheckVarifyEmail) {
     try {
       const payload = await this.jwtService.verifyAsync<{
         email: string;
         code: number;
-      }>(token, {
+      }>(data.token, {
         secret: this.configService.get<string>('AUTHSECRET'),
       });
-
-      if (payload.email != email || payload.code != code) {
+      if (payload.code != data.code) {
         return {
           message: 'Incorrect OTP. Please try again.',
           data: null,
@@ -166,8 +166,7 @@ export class AuthService {
       }
       return {
         message: 'Email verified successfully',
-        error: null,
-        data: payload.email,
+        data: 'Email verified successfully',
         success: true,
         next_page: true,
       };
@@ -175,9 +174,152 @@ export class AuthService {
       console.log(err);
       throw new HttpException(
         {
-          message: 'Incorrect OTP. Please try again. ---- ',
+          message: 'Incorrect OTP. Please try again.',
           data: null,
           success: false,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  // forgetpassword system send email
+  async forgetPasswordGenaredCode(data: VarifyEmailDto) {
+    const userinfo = await this.authUserService.getUserInfo(data.email);
+    if (!userinfo || !userinfo.id) {
+      throw new HttpException(
+        {
+          message: 'User not found',
+          error: 'BAD_REQUEST',
+          data: null,
+          success: false,
+          next_page: false,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const G4code = generateCode();
+    const token = await this.jwtService.signAsync(
+      { userid: userinfo.id, email: userinfo.email, code: G4code },
+      {
+        secret: this.configService.get<string>('AUTHSECRET'),
+        expiresIn: '5m',
+      },
+    );
+    const subject = 'Forget Password';
+    const message = `Your verification code is: ${G4code} </br>
+    or </br>
+    <a href="${process.env.FRONTEND_URL}/forget-password?token=${token}">Click here to reset your password</a>
+    `;
+    const resData = await this.mailService.sendEmail(
+      data.email,
+      subject,
+      message,
+    );
+
+    if (resData.accepted.length < 1 || !resData.response.includes('OK')) {
+      throw new HttpException(
+        {
+          message: 'Email not sent',
+          error: 'BAD_REQUEST',
+          data: null,
+          success: false,
+          next_page: false,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return {
+      message: 'Email sent successfully',
+      error: null,
+      data: { sendCode: resData.messageId, token },
+      redirect_url: `${process.env.FRONTEND_URL}/forget-password?token=${token}`,
+      success: true,
+      next_page: true,
+    };
+  }
+
+  async checkForgetPasswordCode(data: ForgetPasswordCodeCheck) {
+    try {
+      const payload = await this.jwtService.verifyAsync<{
+        userid: string;
+        email: string;
+        code: number;
+      }>(data.token, {
+        secret: this.configService.get<string>('AUTHSECRET'),
+      });
+      if (payload.code != data.code) {
+        return {
+          message: 'Incorrect OTP. Please try again.',
+          data: null,
+          success: false,
+        };
+      }
+      const userinfo = await this.authUserService.getUserInfo(payload.email);
+      if (!userinfo || !userinfo.id) {
+        return {
+          message: 'Incorrect OTP. Please try again.',
+          data: null,
+          success: false,
+        };
+      }
+      const token = await this.jwtService.signAsync(
+        { user: userinfo },
+        {
+          secret: this.configService.get<string>('AUTHSECRET'),
+          expiresIn: '5m',
+        },
+      );
+      return {
+        message: 'Email verified successfully',
+        error: null,
+        data: { chnagePasswordAccessCode: token },
+        chnagePasswordAccess: true,
+        success: true,
+        next_page: true,
+      };
+    } catch {
+      return {
+        message: 'Incorrect OTP. Please try again.',
+        data: null,
+        success: false,
+      };
+    }
+  }
+
+  async changePassword(data: ForgetPasswordToken) {
+    try {
+      const payload = await this.jwtService.verifyAsync<{
+        user: {
+          id: string;
+          email: string;
+          deactivate: boolean;
+          role: string;
+          varify: boolean;
+        };
+        code: number;
+      }>(data.token, {
+        secret: this.configService.get<string>('AUTHSECRET'),
+      });
+
+      const newPass = await this.authUserService.updatePassword(
+        payload.user.id,
+        data.newPassword,
+      );
+      return {
+        message: 'Password changed successfully',
+        error: null,
+        data: newPass.username,
+        success: true,
+      };
+    } catch (err) {
+      console.log(err);
+      throw new HttpException(
+        {
+          message: ' Response time out ',
+          data: null,
+          success: false,
+          redirect_url: 'forget-password',
           next_page: false,
         },
         HttpStatus.UNAUTHORIZED,
