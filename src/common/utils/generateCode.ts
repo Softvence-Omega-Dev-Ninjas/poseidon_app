bef9e139620bb5fa10e96fd210829363208b72e6
mdsharif <srka780@gmail.com>
2025-10-02 00:12:33 +0600

    otp generatecode

--- src/common/utils/generateCode.ts
+++ src/common/utils/generateCode.ts
@@ -3,8 +3,8 @@ export function generateCode() {
   while (true) {
     generateCode = Math.floor(1000 + Math.random() * 9000); // Generates a 4-digit number
     if (
-      generateCode.toString().length > 3 &&
-      generateCode.toString().length < 5
+      generateCode.toString().length > 4 &&
+      generateCode.toString().length < 7
     ) {
       break;
     }
