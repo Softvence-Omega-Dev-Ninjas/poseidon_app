8d0b9adb8a4d97e5044e73d6400004a08660f658
MD Sharif <srka780@gmail.com>
2025-10-01 13:08:12 +0600

    signup dto validation system update

--- src/auth/dto/signup-auth.dto.ts
+++ src/auth/dto/signup-auth.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
 
 export class SignUpUserDto {
   // referral
