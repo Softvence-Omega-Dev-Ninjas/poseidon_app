42d32106b6d3ea73bd603f5ab09abd771307d67d
mdsharif <srka780@gmail.com>
2025-10-07 03:54:33 +0600

    set db video url payment Data

--- src/main/membership/onluUseUserMembershipInfo/useMembershipUser.service.ts
+++ src/main/membership/onluUseUserMembershipInfo/useMembershipUser.service.ts
@@ -38,6 +38,7 @@ export class MembershipServiceUseToUserOnly {
             },
           },
         },
+        zoomUrl: true,
         MembershipSubscriptionPlan: {
           where: {
             duration: membershipLevelInfo.durationType,
