a5181229c6909a19f0088d3f154868e3ced13072
mdsharif <srka780@gmail.com>
2025-10-17 04:10:12 +0600

    web hook load v3

--- src/main/calendly/schedul.service.ts
+++ src/main/calendly/schedul.service.ts
@@ -64,7 +64,7 @@ export class SchedulService {
       await this.prisma.scheduledEvent.create({
         data: {
           email: trck.utm_content,
-          utm_term_userId: trck.utm_term,
+          utm_term_userId: trck.utm_term ?? null,
           salesforce_uuid_bergirlId: trck.salesforce_uuid,
           schedulType_utm_source: 'supportercard', //trck.utm_source,
           // supporterPayId_utm_medium: trck.utm_medium,
