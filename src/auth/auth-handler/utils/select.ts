10e2f3248d2b2829dbea6e7de133fcce7f11cad7
mdsharif <srka780@gmail.com>
2025-10-17 04:31:41 +0600

    web hook load v4

--- src/auth/auth-handler/utils/select.ts
+++ src/auth/auth-handler/utils/select.ts
@@ -1,27 +1,24 @@
 export const userSelect = {
   id: true,
-  email: true,
   provider: true,
   username: true,
+  email: true,
+  password: true,
   role: true,
+  varify: true,
+  stripeAccountId: true,
   profile: {
     select: {
       name: true,
       image: true,
-      cover_image: true,
-      cover_image_offsetY: true,
-      address: true,
-      city: true,
-      country: true,
-      postcode: true,
-      state: true,
-      description: true,
     },
   },
   shop: {
+    select: {
+      id: true,
+    },
+  },
+  memberships_owner: {
     select: { id: true },
   },
-  memberships_owner: { select: { id: true } },
-  stripeAccountId: true,
-  varify: true,
 };
