79448f3df50f0b72a6b1aff34b01e897822ae3cb
mdsharif <srka780@gmail.com>
2025-10-26 06:54:46 +0600

    role base api

--- src/main/contact/contact.controller.ts
+++ src/main/contact/contact.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { Public } from 'src/auth/guard/public.decorator';
-// import { Roles } from 'src/auth/guard/roles.decorator';
-// import { Role } from 'src/auth/guard/role.enum';
+import { Roles } from 'src/auth/guard/roles.decorator';
+import { Role } from 'src/auth/guard/role.enum';
 
 @Controller('contact')
 export class ContactController {
@@ -15,15 +15,13 @@ export class ContactController {
     return this.contactService.create(createContactDto);
   }
 
-  // @Roles(Role.Admin)
-  @Public()
+  @Roles(Role.Admin)
   @Get('get-all')
   findAll() {
     return this.contactService.findAll();
   }
 
-  // @Roles(Role.Admin)
-  @Public()
+  @Roles(Role.Admin)
   @Get('see/:id')
   findOne(@Param('id') id: string) {
     return this.contactService.seeUpdate(id);
